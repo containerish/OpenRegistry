300c93495f4f79cb248b0c33cc523f96b4664d54
guacamole <gunjanwalecha@gmail.com>
2021-11-27 03:02:23 +0530

    Chore: removed ifconfig command with hostname
    Signed-off-by: guacamole <gunjanwalecha@gmail.com>

--- scripts/recreating.sh
+++ scripts/recreating.sh
@@ -8,8 +8,6 @@ set -eu
 
 spec_name=distribution-spec
 spec_version=v1.0.1
-prod_name=example
-trow_version=v0.2.0
 OPENREGISTRY_IMAGE_NAME="openregistry-distribution-build:v$(date +%Y%m%d%H%M%S)"
 
 # check out to OpenRegistry repo
@@ -17,22 +15,21 @@ rm -rf OpenRegistry && git clone git@github.com:containerish/OpenRegistry.git
 pushd OpenRegistry
 docker build -f ./Dockerfile -t "${OPENREGISTRY_IMAGE_NAME}" .
 
-IP=$(ifconfig | grep 192 | awk '{print $2}')
+IP=`hostname -I | awk '{print $1}'`
 sed -in "s/OPEN_REGISTRY_ENVIRONMENT=local/OPEN_REGISTRY_ENVIRONMENT=ci/g" env-vars.example
 sed -in "s/OCI_ROOT_URL=http:\/\/0.0.0.0:5000/OCI_ROOT_URL=http:\/\/$IP:5000/g" conformance.vars
 
-##IP=`hostname -I | awk '{print $1}'`
 echo CI_SYS_ADDR="$IP":5000 >> env-vars.example
 
 docker run --rm -p 5000:5000 --name openregistry -d --env-file=env-vars.example "${OPENREGISTRY_IMAGE_NAME}"
 sleep 5
 curl -XPOST -d '{"email":"johndoe@example.com","username":"johndoe","password":"Qwerty@123"}' "http://0.0.0.0:5000/auth/signup"
 popd
+
 # check out conformance repo
 rm -rf conf-tmp && git clone https://github.com/opencontainers/${spec_name}.git conf-tmp
 pushd conf-tmp/conformance && docker build -t conformance:latest -f Dockerfile .
 popd
-
 
 docker run --rm \
   -v $(pwd)/results:/results \
