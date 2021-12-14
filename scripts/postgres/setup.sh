e1b81b431668b02ce40edbcfccc25e6f11526003
guacamole <gunjanwalecha@gmail.com>
2021-12-14 12:10:04 +0530

    Fix: attempt to fix github actions to include postgres config
    Signed-off-by: guacamole <gunjanwalecha@gmail.com>
    Signed-off-by: jay-dee7 <jasdeepsingh.uppal@gmail.com>

--- scripts/postgres/setup.sh
+++ scripts/postgres/setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 set -e
-/Applications/Postgres.app/Contents/Versions/14/bin/psql -v ON_ERROR_STOP=1 --username "$PGUSER" --dbname "$PGDATABASE" <<-EOSQL
-	CREATE USER jane_doe;
-    GRANT ALL PRIVILEGES ON DATABASE open_registry TO jane_doe;
+psql -v ON_ERROR_STOP=1 --host="0.0.0.0" --port="5432" --username "$PGUSER" --dbname "$PGDATABASE" <<-EOSQL
 	\c open_registry;
 	\i scripts/postgres/OpenRegistry.sql
 EOSQL
