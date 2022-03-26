c64ddec7e82d540b97448ca9efb985785357b170
guacamole <gunjanwalecha@gmail.com>
2022-03-26 20:15:01 +0530

    Fix: modified the format of error and added yellow color for warn level

--- telemetry/consoleWriter.go
+++ telemetry/consoleWriter.go
@@ -15,7 +15,7 @@ import (
 
 func (l logger) consoleWriter(ctx echo.Context) {
 	l.zlog = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC822})
-	l.zlog = l.zlog.With().Caller().Logger()
+	l.zlog = l.zlog.With().Logger()
 
 	buf := l.pool.Get().(*bytes.Buffer)
 	buf.Reset()
@@ -37,52 +37,39 @@ func (l logger) consoleWriter(ctx echo.Context) {
 
 	var e multierror.Error
 
-	_, err := buf.WriteString(req.Host + " ")
-	if err != nil {
+	_, err := buf.WriteString(req.Method + " ")
+	e.Errors = append(e.Errors, err)
+
+	_, err = buf.WriteString(color.GreenString("%d ", res.Status))
+	e.Errors = append(e.Errors, err)
+
+	if level == zerolog.ErrorLevel {
+		e.Errors = append(e.Errors, err)
+	}
+	if level == zerolog.WarnLevel {
 		e.Errors = append(e.Errors, err)
 	}
 
-	_, err = buf.WriteString(req.Method + " ")
-	if err != nil {
-		e.Errors = append(e.Errors, err)
-	}
+	_, err = buf.WriteString(req.Host)
+	e.Errors = append(e.Errors, err)
 
 	_, err = buf.WriteString(req.RequestURI + " ")
-	if err != nil {
-		e.Errors = append(e.Errors, err)
-	}
+	e.Errors = append(e.Errors, err)
 
 	_, err = buf.WriteString(req.Proto + " ")
-	if err != nil {
-		e.Errors = append(e.Errors, err)
-	}
+	e.Errors = append(e.Errors, err)
 
 	_, err = buf.WriteString(req.UserAgent() + " ")
-	if err != nil {
-		e.Errors = append(e.Errors, err)
-	}
-
-	if level == zerolog.InfoLevel {
-		_, err = buf.WriteString(color.GreenString(" %d", res.Status))
-		if err != nil {
-			e.Errors = append(e.Errors, err)
-		}
-	} else {
-		_, err = buf.WriteString(color.RedString(" %d", res.Status))
-		if err != nil {
-			e.Errors = append(e.Errors, err)
-		}
-	}
+	e.Errors = append(e.Errors, err)
 
 	if ctxErr, ok := ctx.Get(types.HttpEndpointErrorKey).(string); ok {
 		_, err = buf.WriteString(color.YellowString(" %s", ctxErr))
-		if err != nil {
-			e.Errors = append(e.Errors, err)
-		}
+		e.Errors = append(e.Errors, err)
 	}
 
-	if err != nil {
-		buf.WriteString(err.Error())
+	if err := e.ErrorOrNil(); err != nil {
+		buf.WriteString(e.Error())
 	}
+
 	l.zlog.WithLevel(level).Msg(buf.String())
 }
