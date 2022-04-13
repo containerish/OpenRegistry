3505b204086d8645a0f2fe0ec748402e02636a99
jay-dee7 <jasdeepsingh.uppal@gmail.com>
2022-04-13 21:17:13 +0530

    Add: read configuration from environment variable
    Signed-off-by: jay-dee7 <jasdeepsingh.uppal@gmail.com>

--- config/yaml.go
+++ config/yaml.go
@@ -1,6 +1,12 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"os"
+
+	"github.com/fatih/color"
+	"github.com/spf13/viper"
+	"gopkg.in/yaml.v2"
+)
 
 func ReadYamlConfig() (*OpenRegistryConfig, error) {
 	viper.SetConfigName("config")
@@ -8,11 +14,22 @@ func ReadYamlConfig() (*OpenRegistryConfig, error) {
 	viper.AddConfigPath(".")
 	viper.AddConfigPath("$HOME/.openregistry")
 
+	var registryConfig OpenRegistryConfig
+	// OPENREGISTRY_CONFIG env variable takes precedence over everything
+	if yamlConfigInEnv := os.Getenv("OPENREGISTRY_CONFIG"); yamlConfigInEnv != "" {
+		err := yaml.Unmarshal([]byte(yamlConfigInEnv), &registryConfig)
+		if err != nil {
+			return nil, err
+		}
+
+		color.Green("read configuration from environment variable")
+		return &registryConfig, nil
+	}
+
 	if err := viper.ReadInConfig(); err != nil {
 		return nil, err
 	}
 
-	var registryConfig OpenRegistryConfig
 	if err := viper.Unmarshal(&registryConfig); err != nil {
 		return nil, err
 	}
